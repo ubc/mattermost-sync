0f6eb3cab35d4ea506261b7072abf678454c1470
Pan Luo <pan.luo@ubc.ca>
2018-09-08 23:21:08 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Mattermost-Sync',
+   version='1.0',
+   description='Sync LDAP groups to Mattermost team',
+   author='Pan Luo',
+   author_email='pan.luo@ubc.ca',
+   packages=['mattermost-sync'],  #same as name
+)
